1c2dd2f83c56a83086f73ca83694ccf1f231e155
Naimad94 <55095623+Naimad94@users.noreply.github.com>
2023-09-28 00:59:00 -0400

    Stuck on PG 507
    Confused about headers in Pg 507 a. and Pg 507 2 don't understand what is being asked. Also gfetting an error code about namespace.

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -4,17 +4,49 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //fierldsd set in the Unity Inspector pane
+    [Header ("Set in Inspector")]
+    public GameObject prefabProjectile;
+
+    //field set dynamically
+    [Header ("Set Dynamically")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("launchPoint"); //transform.Find ("LaunchPoint") sarches for a child of slingshot named LaunchPoint and returns its transform.
+        launchPoint = launchPointTrans.gameObject;  //Gets the game object associated with that transform and assigns it to the GameObject field launchPoint.
+        launchPoint.SetActive(false); //The SetActive method on GameObject like launchPoint tells the game whether or not to ignore them. 
+        launchPos = launchPointTrans.position;
+    }
+
     void OnMouseEnter()
     {
-        print("Slingshot: OnMouseEnter()");
+        print("Slingshot: OnMouseEnter()"); //print ("Slingshot: OnMouseEnter()");
+        launchPoint.SetActive(true);
     }
 
     // Update is called once per frame
     void OnMouseExit()
     {
-        print("Slingshot: OnMouseExit()");  
+        print("Slingshot: OnMouseExit()");  //print ("Slingshot: OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        //The player has pressed the mouse button while over Slingshot
+        aimingMode = true;
+        //Instantiate a Projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        //Start it at the launchPoint
+        projectile.transform.position = launchPos;
+        //Set it as kinematic for now
+        projectile.GetComponent<RigidBody>().isKinematic = true;
     }
 }
 
-//What is monodevelop?
+//What is mono develop?
