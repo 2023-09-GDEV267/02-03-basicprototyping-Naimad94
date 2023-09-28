5859445dafdef326e28146d4b84920d1f9077504
Naimad94 <55095623+Naimad94@users.noreply.github.com>
2023-09-27 23:59:35 -0400

    Coding Slingshot Class

---
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnMouseEnter()
+    {
+        print("Slingshot: OnMouseEnter()");
+    }
+
+    // Update is called once per frame
+    void OnMouseExit()
+    {
+        print("Slingshot: OnMouseExit()");  
+    }
+}
+
+//What is monodevelop?
